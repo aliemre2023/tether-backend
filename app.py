4a341b2bb9c968677a46accb2ed39dfe75dfd34d
onat <onatbudak@gmail.com>
2025-05-03 10:45:15 +0300

    moved

--- app.py
+++ app.py
@@ -9,22 +9,29 @@ triangles = [[] for _ in range(24)]
 def move(tri_id, dice_value):
     from_index = tri_id
     to_index = tri_id + dice_value
+    print(from_index)
+    
 
-    if from_index < 0 or to_index >= len(triangles):
+    if from_index < 0 or to_index >= 24:
+        print("index")
         return 0
 
     source_stack = triangles[from_index]
     target_stack = triangles[to_index]
-
-    source_color = source_stack[-1]
-    target_color = target_stack[-1]
+    print(triangles[from_index])
+    print(triangles[to_index])
 
     if not source_stack:
+        print("empty")
         return 0
 
-    if (len(target_stack) == 0 or len(target_stack) == 1) or target_color == source_color:
-        return 1
+    source_color = source_stack[-1]
+    target_color = target_stack[-1] if target_stack else None
 
+    if (len(target_stack) == 0 or len(target_stack) == 1) or target_color == source_color:
+        print("tas")
+        return 1
+    
     return 0
 
 @app.route('/api/reset', methods=['GET'])
@@ -48,19 +55,25 @@ def get_move():
     data = request.get_json()
 
     try:
-        triangle_id = int(data.get("tri_id"))
+        triangle_id = int(data.get("index"))
         dice_1 = int(data.get("dice1"))
         dice_2 = int(data.get("dice2"))
     except (TypeError, ValueError):
         return jsonify({"error": "Invalid input"}), 400
+    
+    if triangle_id <=5:
+        triangle_id = 5-triangle_id
+
+    if triangle_id >5 and triangle_id <= 11:
+        triangle_id = 11-triangle_id+6
 
     moves = []
 
-    if move(triangle_id, dice_1):
+    if move(triangle_id, dice_1) == 1:
         moves.append(triangle_id + dice_1)
-    if move(triangle_id, dice_2):
+    if move(triangle_id, dice_2) == 1:
         moves.append(triangle_id + dice_2)
-    if move(triangle_id, dice_1 + dice_2):
+    if move(triangle_id, dice_1 + dice_2) == 1:
         moves.append(triangle_id + dice_1 + dice_2)
 
     return jsonify({"moves": moves})
